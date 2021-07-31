4ca04f956cd80d67150a1df9aac46fa6a2b60356
Garrett Harty <gharty@live.com>
2021-07-31 10:09:55 -0500

    setup server listen and required express and body parser

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
 // require express and body-parser
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// create app
+const app = express();
+
+// global variables
+const port = 5000;
+
+// listen for requests
+app.listen(port, () => {
+    console.log('App is up and running');
+});
