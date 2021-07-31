a83d7f88e5ef5a2bc25daf7c2181e12b807b1302
Garrett Harty <gharty@live.com>
2021-07-31 10:19:22 -0500

    setup get and post serverside

--- server/server.js
+++ server/server.js
@@ -4,9 +4,28 @@ const bodyParser = require('body-parser');
 
 // create app
 const app = express();
+app.use(express.static('./server/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // global variables
 const port = 5000;
+let solution = {
+    solution: 0
+};
+
+// setup get request
+app.get('/calculator', (req, res) => {
+    console.log('Ready to send back calculations');
+    console.log('Route is:', req.route.path);
+    res.send(solution);
+});
+
+//setup post request
+app.post('/inputs', (req, res) => {
+    console.log('Ready to post user input');
+    res.sendStatus(200);
+});
 
 // listen for requests
 app.listen(port, () => {
