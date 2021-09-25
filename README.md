a255fb32b821cc0d0bcd44f4ac2616e90950ac3d
Garrett Harty <gharty@live.com>
2021-09-25 16:52:11 -0500

    adjusted styling

--- README.md
+++ README.md
@@ -6,43 +6,8 @@ jquery and node Server Calculator Project
 
 Creating a server app that functions as a calculator and stores all calculator uses on the DOM.
 
-## To-do List
+## Images
 
-[x] Setup
-    [x] index.html
-        [x] source in jquery / script.js
-        [X] link style sheet
-    [x] install express and body-parser
-    [x] server setup
-        [x] require express
-        [x] require body-parser
-        [x] setup listen
-
-[x] html layout
-    [x] headers and ul for storing calculations
-    [x] input fields for each number
-    [x] 4 operator buttons
-    [x] = button
-    [x] clear button
-
-[x] server code
-    [x] setup a get
-    [x] setup a post
-    [x] add logic operations for computing the answer
-
-[x] client setup
-    [x] setup onReady
-    [x] setup get // should be what actually displays the calculation
-    [x] setup post // for sending the inputs and the operator
-
-## Stretch
-
-[x] add buttons for integers 0-9
-[x] add decimal button
-[x] add live display of buttons pressed
-
-[] livedisplay functionality
-    [] button presses must cocatenate when pressed
 
 
 
--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static('./server/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // global variables
 const port = 5000;
@@ -21,7 +23,7 @@ let currentSolution = {
 };
 
 // calculate out the real current solution
-function calcSolution(array){
+function calcSolution(array) {
     let firstObject = array[0];
     switch (true) {
         case firstObject.operator === '+':
@@ -39,11 +41,17 @@ function calcSolution(array){
         case firstObject.operator === '^':
             return Number(firstObject.num1) ** Number(firstObject.num2);
             break;
+        case firstObject.operator === '^':
+            return Number(firstObject.num1) ** Number(firstObject.num2);
+            break;
+        case firstObject.operator === '^':
+            return Number(firstObject.num1) ** Number(firstObject.num2);
+            break;
     };
 };
 
 //using the calcSolution, add a key/value pair to the object
-function addKeyValuePair(array){
+function addKeyValuePair(array) {
     let firstObject = array[0];
     firstObject.solution = calcSolution(array);
 }
@@ -54,7 +62,7 @@ app.get('/calcHistory', (req, res) => {
     console.log('Route is:', req.route.path);
 
     // determine if calcArray has an object, and if so assign a new keyvalue with our current solution
-    if (calcArray.length > 0){
+    if (calcArray.length > 0) {
         addKeyValuePair(calcArray);
     };
     console.log(calcArray[0]);
@@ -64,7 +72,7 @@ app.get('/calcHistory', (req, res) => {
 //setup get request for displaying currentSolution
 app.get('/solution', (req, res) => {
     console.log('Ready to send back solution');
-    
+
     // assign current solution
     currentSolution.currentSolutionVal = calcSolution(calcArray).toFixed(2);
     res.send(currentSolution);
@@ -73,14 +81,14 @@ app.get('/solution', (req, res) => {
 //setup post request
 app.post('/calcHistory', (req, res) => {
     console.log('Ready to post user input');
-    
+
     // log data from client
     console.log('req.body', req.body);
 
     let newCalculation = req.body;
 
     // validate user inputs
-    if (!newCalculation.num1 || !newCalculation.num2 || newCalculation.operator === ''){
+    if (!newCalculation.num1 || !newCalculation.num2 || newCalculation.operator === '') {
         // user has missed an input, indicate client error
         res.status(400).send({
             message: 'Missing required field or operator. Please try again.'
